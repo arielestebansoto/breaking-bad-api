b85cc4bac66db342cfed0fe00aa3274e42df0408
ariel soto <arielestebansoto@gmail.com>
2021-08-03 19:40:26 -0300

    change to white-text in page loading and color change in the scroll bar

--- js/PageLoading.js
+++ js/PageLoading.js
@@ -3,8 +3,10 @@ const app = document.querySelector('#app')
 function PageLoading() {
     app.innerHTML=`
     <div class="container">
-        <div class="row center"  >
-            <h5>Cooking...</h5>
+        <div class="row center" >
+            <div class="col s12">
+                <h4 class="white-text">Cooking...</h4>
+            </div>
         </div>
     </div>
     `
