30667970b497ac1fbea4947833ac8122f75987ee
ariel <arielestebansoto@gmail.com>
2021-09-29 07:55:29 -0300

    Characters Card component

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,22 @@
 export const CharacterCard = (character) => {
-    return `
+    const Card = document.createElement('div')
+    Card.classList.add('col', 's12', 'm6', 'l4')
     
-    `
+    Card.innerHTML=` 
+        <div class="card small z-depth-5">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img src=${character.img}>
+                <span class="card-title activator white-text text-darken-4">${character.name}<i class="material-icons right">more_vert</i></span>
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">${character.name}<i class="material-icons right activator">close</i></span>
+                <p>Nick Name: "${character.nickname}".</p>
+                <p>Ocupation: ${character.occupation.join(', ')}.</p>
+                <p>State: ${character.status}.</p>
+                <p>Seasons: ${character.appearance.join(', ')}.</p>
+                <p>Actor: ${character.portrayed}.</p>
+            </div>
+        </div>`
+
+    return Card
 }
--- src/components/CharactersContainer.js
+++ src/components/CharactersContainer.js
@@ -1,10 +1,11 @@
 import { getCharacters } from "../utils/getCharacters.js"
+import { CharacterCard } from "./CharacterCard.js"
 
 export const CharactersContainer =  async () => {
     const Container = document.querySelector('#characters-container')
 
     const data = await getCharacters()
 
-    data.forEach( (element, index ) => Container.append(element.name))
+    data.forEach( character => Container.append(CharacterCard(character)))
 }
 
